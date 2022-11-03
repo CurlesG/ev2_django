4f330149f6410eb56e9b9c56dad28e60f7a4c3bb
CurlesG <96333280+CurlesG@users.noreply.github.com>
2022-11-03 00:37:53 -0300

    Update 0001_initial.py

--- app_libros/migrations/0001_initial.py
+++ app_libros/migrations/0001_initial.py
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=100, unique=True)),
-                ('date_of_birth', models.DateField(default=sqlalchemy.sql.expression.null)),
+                ('date_of_birth', models.DateField()),
             ],
         ),
         migrations.CreateModel(
